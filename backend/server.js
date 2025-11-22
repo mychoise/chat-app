b247044e613ae1d56f510143ded1bd0983ece017
Sabin Dhakal <137175074+mychoise@users.noreply.github.com>
2025-11-22 12:09:32 +0400

    Update CORS origin to production URL

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json()); // Parses incoming JSON requests
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["https://chat-app-sabin.netlify.app/"],
     credentials:true,
 }))
 
